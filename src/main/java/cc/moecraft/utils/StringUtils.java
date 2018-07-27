6ff6302bb83d9d705324dcb790e900f6a3b67f88
Hykilpikonna <admin@moecraft.cc>
2018-07-27 19:34:01 +0800

    [+] StringUtils字符串工具类

---
+++ src/main/java/cc/moecraft/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package cc.moecraft.utils;
+
+import java.util.Scanner;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/24 创建!
+ * Created by Hykilpikonna on 2018/04/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class StringUtils
+{
+}
