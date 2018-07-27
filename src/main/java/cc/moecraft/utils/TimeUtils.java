84e7e4c52352e43a8b1cee68d06d6c72aa6a3442
Hykilpikonna <admin@moecraft.cc>
2018-07-27 19:36:33 +0800

    [+] 转换时间为字符串

--- src/main/java/cc/moecraft/utils/TimeUtils.java
+++ src/main/java/cc/moecraft/utils/TimeUtils.java
@@ -27,6 +27,25 @@ public class TimeUtils
     }
 
     /**
+     * 转换时间为字符串
+     *
+     * 时间格式和Date的格式一样
+     *
+     * @param format 字符串格式
+     * @param time 时间
+     * @param timeUnit 时间单位
+     * @return 字符串
+     */
+    public static String convertToString(String format, float time, TimeUnits timeUnit)
+    {
+        long millisTime = Math.round(time * TimeUnits.getTotalMultiplierBetweenTwoUnits(timeUnit, TimeUnits.Millis));
+
+        Date date = new Date(millisTime);
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(date);
+    }
+
+    /**
      * 时间单位
      */
     public enum TimeUnits
