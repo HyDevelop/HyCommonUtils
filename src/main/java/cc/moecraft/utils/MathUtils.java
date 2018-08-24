266377a053db9d577a26016b71f3752ebf9a5cd8
Hykilpikonna <admin@moecraft.cc>
2018-08-24 14:32:07 -0400

    [+] 数学工具类MathUtils

---
+++ src/main/java/cc/moecraft/utils/MathUtils.java
@@ -0,0 +1,15 @@
+package cc.moecraft.utils;
+
+import java.util.Random;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/24 创建!
+ * Created by Hykilpikonna on 2018/08/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class MathUtils
+{
+}
