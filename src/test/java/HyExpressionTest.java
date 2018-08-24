b4e11899c66bbcb92c69c334deef461825358bcd
Hykilpikonna <admin@moecraft.cc>
2018-08-24 14:34:06 -0400

    [T] 测试HyExpression

---
+++ src/test/java/HyExpressionTest.java
@@ -0,0 +1,21 @@
+import cc.moecraft.utils.HyExpressionResolver;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/24 创建!
+ * Created by Hykilpikonna on 2018/08/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class HyExpressionTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(HyExpressionResolver.resolve("今天%rs{很不错,很差,很もえ,很适合写一天代码,很适合玩TH14 EX,很适合删库跑路}"));
+        System.out.println(HyExpressionResolver.resolve("您的暑假余额不足, 还剩%ri{-100,-1}天, 请充值"));
+        System.out.println(HyExpressionResolver.resolve("抽到了: %rp{SSR卡,3,SR卡,17,R卡,40,N卡,40}"));
+        System.out.println(HyExpressionResolver.resolve("%var{a,100}%var{b,50}%cal{(a+b)*b}"));
+        System.out.println(HyExpressionResolver.resolve("%var{a,%ri{1,100}}%var{b,%rd{0,1}}%cal{(a+b)*(a+b)*%rd{1,2,4}}"));
+    }
+}
