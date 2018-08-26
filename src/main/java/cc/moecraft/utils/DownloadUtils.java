b75bf0f8e87f598a82ff28f9c02125a5b95a5c0d
Hykilpikonna <admin@moecraft.cc>
2018-08-26 16:43:44 -0400

    [+] 下载工具类

---
+++ src/main/java/cc/moecraft/utils/DownloadUtils.java
@@ -0,0 +1,21 @@
+package cc.moecraft.utils;
+
+import cn.hutool.http.HttpUtil;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.nio.charset.Charset;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/26 创建!
+ * Created by Hykilpikonna on 2018/08/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class DownloadUtils
+{
+}
