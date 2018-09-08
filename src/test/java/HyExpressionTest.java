c794b5bfa326f719efe422c8deb3819bee198fb1
Hykilpikonna <admin@moecraft.cc>
2018-09-08 17:22:05 -0400

    [+] 测试

--- src/test/java/HyExpressionTest.java
+++ src/test/java/HyExpressionTest.java
@@ -12,15 +12,16 @@ public class HyExpressionTest
 {
     public static void main(String[] args)
     {
-        System.out.println(HyExpressionResolver.resolve("今天%rs{很不错,很差,很もえ,很适合写一天代码,很适合玩TH14 EX,很适合删库跑路}"));
-        System.out.println(HyExpressionResolver.resolve("您的暑假余额不足, 还剩%ri{-100,-1}天, 请充值"));
-        System.out.println(HyExpressionResolver.resolve("抽到了: %rp{SSR卡,3,SR卡,17,R卡,40,N卡,40}"));
-        System.out.println(HyExpressionResolver.resolve("%var{a,100}%var{b,50}%cal{(a+b)*b}"));
-        System.out.println(HyExpressionResolver.resolve("%var{a,%ri{1,100}}%var{b,%rd{0,1}}%cal{(a+b)*(a+b)*%rd{1,2,4}}"));
+        HyExpressionResolver resolver = new HyExpressionResolver(false);
 
+        System.out.println("---------- 没有SafeMode ----------");
+        test(resolver);
 
-        System.out.println(HyExpressionResolver.resolve("%rp{A卡,-1,N卡,-1}"));
-        System.out.println(HyExpressionResolver.resolve("%rp{A卡,100,N卡,0}"));
+        resolver.setSafeMode(true);
+        System.out.println();
+
+        System.out.println("---------- 有SafeMode ----------");
+        test(resolver);
     }
 
     private static void test(HyExpressionResolver resolver)
