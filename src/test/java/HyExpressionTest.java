de86da03aff1b8d8c4ee72e9f0af0c199191ec06
Hykilpikonna <admin@moecraft.cc>
2018-09-08 17:21:59 -0400

    [+] 封装测试方法

--- src/test/java/HyExpressionTest.java
+++ src/test/java/HyExpressionTest.java
@@ -22,4 +22,17 @@ public class HyExpressionTest
         System.out.println(HyExpressionResolver.resolve("%rp{A卡,-1,N卡,-1}"));
         System.out.println(HyExpressionResolver.resolve("%rp{A卡,100,N卡,0}"));
     }
+
+    private static void test(HyExpressionResolver resolver)
+    {
+        System.out.println(resolver.resolve("今天%rs{很不错,很差,很もえ,很适合写一天代码,很适合玩TH14 EX,很适合删库跑路}"));
+        System.out.println(resolver.resolve("您的暑假余额不足, 还剩%ri{-100,-1}天, 请充值"));
+        System.out.println(resolver.resolve("抽到了: %rp{SSR卡,3,SR卡,17,R卡,40,N卡,40}"));
+        System.out.println(resolver.resolve("%var{a,100}%var{b,50}%cal{(a+b)*b}"));
+        System.out.println(resolver.resolve("%var{a,%ri{1,100}}%var{b,%rd{0,1}}%cal{(a+b)*(a+b)*%rd{1,2,4}}"));
+
+
+        System.out.println(resolver.resolve("%rp{A卡,-1,N卡,-1}"));
+        System.out.println(resolver.resolve("%rp{A卡,100,N卡,0}"));
+    }
 }
