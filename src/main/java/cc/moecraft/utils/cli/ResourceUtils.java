bd1308733e358e9764a90fdd53b1b78673a82a4a
Hykilpikonna <admin@moecraft.cc>
2018-09-16 10:46:14 -0400

    [+] 资源工具类

---
+++ src/main/java/cc/moecraft/utils/cli/ResourceUtils.java
@@ -0,0 +1,22 @@
+package cc.moecraft.utils.cli;
+
+import cc.moecraft.utils.ClassLoaderUtils;
+import cc.moecraft.utils.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@SuppressWarnings("WeakerAccess")
+public class ResourceUtils
+{
+}
