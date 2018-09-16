c422b7d5624fab1c836c77ae75bf3b07fb675c82
Hykilpikonna <admin@moecraft.cc>
2018-09-16 10:13:20 -0400

    [+] Args参数类

---
+++ src/main/java/cc/moecraft/utils/cli/Args.java
@@ -0,0 +1,22 @@
+package cc.moecraft.utils.cli;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data @AllArgsConstructor
+public class Args
+{
+    private ArrayList<String> operations;
+    private Map<String, String> options;
+}
