d4fb137c090969d7a813f01f78f6836457068943
Hykilpikonna <admin@moecraft.cc>
2018-09-16 11:30:35 -0400

    [+] 把所有变量输出到一个字符串里

--- src/main/java/cc/moecraft/utils/ExceptionUtils.java
+++ src/main/java/cc/moecraft/utils/ExceptionUtils.java
@@ -12,4 +12,28 @@ import java.lang.reflect.Field;
  */
 public class ExceptionUtils
 {
+    /**
+     * 把所有变量输出到字符串里
+     * @param throwable 异常
+     * @param <T> 类型
+     * @return 字符串
+     */
+    public static <T extends Throwable> String getAllVariables(T throwable)
+    {
+        StringBuilder variables = new StringBuilder("@").append(throwable.getClass().getName()).append("[");
+
+        for (Field field : throwable.getClass().getDeclaredFields())
+        {
+            field.setAccessible(true);
+            try
+            {
+                String name = field.getName();
+                String value = field.get(throwable).toString();
+                variables.append(name).append("=").append(value).append(";");
+            }
+            catch (IllegalAccessException ignored) {}
+        }
+
+        return variables.append("]").toString();
+    }
 }
