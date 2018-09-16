948c48faf6c46a9d814903a8ee2ba758e7abe3ec
Hykilpikonna <admin@moecraft.cc>
2018-09-16 10:50:13 -0400

    [T] 测试

---
+++ src/test/java/CLIUtilsTest.java
@@ -0,0 +1,17 @@
+import cc.moecraft.utils.cli.ResourceUtils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class CLIUtilsTest
+{
+    public static void main(String[] args)
+    {
+        ResourceUtils.printResource(System.out::println, "test.txt");
+    }
+}
