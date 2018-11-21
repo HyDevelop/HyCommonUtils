9c644bc4899101999ee9e1125a76e9428e6be873
Hykilpikonna <admin@moecraft.cc>
2018-11-20 19:48:42 -0500

    [+] Processor and preProcessor interface

---
+++ src/main/java/cc/moecraft/utils/hyexp/HyExpProcessor.java
@@ -0,0 +1,14 @@
+package cc.moecraft.utils.hyexp;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/11/20 创建!
+ * Created by Hykilpikonna on 2018/11/20!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+interface HyExpProcessor
+{
+    String process(String[] tags);
+}
