c1c6540b40ebeea6b4f3c68ce66684bf5cbe6958
Hykilpikonna <admin@moecraft.cc>
2018-12-01 21:02:23 -0500

    [T] Test codec

---
+++ src/test/java/CodecTest.java
@@ -0,0 +1,18 @@
+import cc.moecraft.utils.StringCodecUtils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/01 创建!
+ * Created by Hykilpikonna on 2018/12/01!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class CodecTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(StringCodecUtils.toHex("123 Hello World! 中文"));
+        System.out.println(StringCodecUtils.fromHex("31 32 33 20 48 65 6C 6C 6F 20 57 6F 72 6C 64 21 20 E4 B8 AD E6 96 87"));
+    }
+}
