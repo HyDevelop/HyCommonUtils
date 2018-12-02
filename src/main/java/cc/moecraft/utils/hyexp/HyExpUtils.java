480cb6f48a6449749e27a9a62250b86ba7eb19f6
Hykilpikonna <admin@moecraft.cc>
2018-12-01 21:26:04 -0500

    [+] Process a tag without a return value

--- src/main/java/cc/moecraft/utils/hyexp/HyExpUtils.java
+++ src/main/java/cc/moecraft/utils/hyexp/HyExpUtils.java
@@ -2,6 +2,8 @@ package cc.moecraft.utils.hyexp;
 
 import java.util.regex.Matcher;
 
+import static cc.moecraft.utils.hyexp.HyExpProcessors.*;
+
 /**
  * 此类由 Hykilpikonna 在 2018/11/20 创建!
  * Created by Hykilpikonna on 2018/11/20!
@@ -40,5 +42,23 @@ class HyExpUtils
         while (matcher.find()) original = pattern.replace.matcher(original).replaceFirst(
                 String.valueOf(processor.process(preProcessor.preProcess(matcher.group()).split(","))));
         return original;
+    }
+
+    /**
+     * Process a tag.
+     *
+     * @param pattern Pattern
+     * @param original Original String.
+     * @param preProcessor Processor before the tag is split.
+     * @param processor Processor for processing the tag.
+     * @return Result.
+     */
+    static String processDo(HyExpPattern pattern, String original, HyExpPreProcessor preProcessor, HyExpDoer processor)
+    {
+        return process(pattern, original, preProcessor, tags ->
+        {
+            processor.process(tags);
+            return "";
+        });
     }
 }
