ef8e3a29234d590c22d226243a693eebd9358f5f
Hykilpikonna <admin@moecraft.cc>
2018-12-01 21:25:00 -0500

    [M] Move HyExpProcessors into one class.

---
+++ src/main/java/cc/moecraft/utils/hyexp/HyExpProcessors.java
@@ -0,0 +1,27 @@
+package cc.moecraft.utils.hyexp;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/11/20 创建!
+ * Created by Hykilpikonna on 2018/11/20!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+class HyExpProcessors
+{
+    interface HyExpPreProcessor
+    {
+        String preProcess(String raw);
+    }
+
+    interface HyExpProcessor
+    {
+        Object process(String[] tags);
+    }
+
+    interface HyExpDoer
+    {
+        void process(String[] tags);
+    }
+}
