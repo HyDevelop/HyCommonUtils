92fffd12ee6022cdae1dc21d87f0407e51c9d4e0
Hykilpikonna <admin@moecraft.cc>
2018-12-01 21:59:36 -0500

    [+] Map utils

---
+++ src/main/java/cc/moecraft/utils/MapUtils.java
@@ -0,0 +1,18 @@
+package cc.moecraft.utils;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/01 创建!
+ * Created by Hykilpikonna on 2018/12/01!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class MapUtils
+{
+}
