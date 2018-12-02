69c47d12a18bd5b5f12ba550c814f85b8bc8c6db
Hykilpikonna <admin@moecraft.cc>
2018-12-01 21:21:29 -0500

    [T] Test ascii encoding

--- src/test/java/CodecTest.java
+++ src/test/java/CodecTest.java
@@ -14,5 +14,8 @@ public class CodecTest
     {
         System.out.println(StringCodecUtils.toHex("123 Hello World! 中文"));
         System.out.println(StringCodecUtils.fromHex("31 32 33 20 48 65 6C 6C 6F 20 57 6F 72 6C 64 21 20 E4 B8 AD E6 96 87"));
+
+        System.out.println(StringCodecUtils.toAscii("123 Hello World! 中文"));
+        System.out.println(StringCodecUtils.fromAscii("49 50 51 32 72 101 108 108 111 32 87 111 114 108 100 33 32 20013 25991"));
     }
 }
